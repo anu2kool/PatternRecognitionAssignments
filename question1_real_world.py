57bb4b0129328bdf75455a3d3e0d04e1e776f95c
Divyansh Vinayak <b19080@students.iitmandi.ac.in>
2021-09-30 07:37:00 +0530

    Add question 1 and format question 2

---
+++ question1_real_world.py
@@ -0,0 +1,103 @@
+# importing necessary libraries
+import numpy as np  # for mathematical operations
+import pandas as pd  # for reading and handling the data
+import matplotlib.pyplot as plt  # for plotting and visualising the dataset
+from sklearn.metrics import accuracy_score, confusion_matrix  # for obtaining accuracy score and confusion matrix
+from sklearn.model_selection import train_test_split  # for splitting the dataset into training and test data
+
+
+def solve(data_path):
+    # reading the data and storing different classes as class1 and class2
+    class1 = pd.read_csv(data_path + "/class1.txt", delimiter=' ', names=['column1', 'column2'])
+    class2 = pd.read_csv(data_path + "/class2.txt", delimiter=' ', names=['column1', 'column2'])
+    class3 = pd.read_csv(data_path + "/class3.txt", delimiter=' ', names=['column1', 'column2'])
+
+    # clean data
+    class1 = class1.reset_index()
+    class1.drop('column2', inplace=True, axis=1)
+    class1.rename(columns={'index': 'column1', 'column1': 'column2'}, inplace=True)
+
+    class2 = class2.reset_index()
+    class2.drop('column2', inplace=True, axis=1)
+    class2.rename(columns={'index': 'column1', 'column1': 'column2'}, inplace=True)
+
+    class3 = class3.reset_index()
+    class3.drop('column2', inplace=True, axis=1)
+    class3.rename(columns={'index': 'column1', 'column1': 'column2'}, inplace=True)
+
+    # data consists of two features
+    # adding the new column which is the label/class for that data point
+    # label 0 for class1
+    # label 1 for class2
+    # label 2 for class3
+    class1['class'] = 0
+    class2['class'] = 1
+    class3['class'] = 2
+
+    # merging the data for both classes
+    merged_data = pd.concat([class1, class2, class3])
+
+    # resetting the indices for all the entries
+    merged_data = merged_data.reset_index(drop=True)
+
+    # extracting the 'class' column
+    y = merged_data['class']
+
+    # dropping the 'class' column
+    data = merged_data.drop(['class'], axis=1)
+
+    # splitting the dataset into train and test data in the ratio of 80% to 20%
+    X_train, X_test, Y_train, Y_test = train_test_split(data, y, test_size=0.2)
+
+    # getting the unique class labels and its count
+    classes = np.unique(y)
+    count = len(classes)
+
+    def get_params(features, target):
+        """ Compute the parameters of Gaussian distribution.
+        """
+        mean = features.groupby(target).apply(np.mean).to_numpy()
+        var = features.groupby(target).apply(np.var).to_numpy()
+        return mean, var
+
+    def get_likelihood(x, mean, var):
+        """ Compute the likelihood of data vector x from the given parameters of Gaussian distribution.
+        """
+        return np.exp(-(x - mean) ** 2 / (2 * var)) / np.sqrt(2 * np.pi * var)
+
+    def get_priors(features, target):
+        """ Compute the prior for each class.
+        """
+        rows = features.shape[0]
+        prior = (features.groupby(target).apply(lambda x: len(x)) / rows).to_numpy()
+        return prior
+
+    def get_posteriors(x, prior, mean, var):
+        """ Compute the posterior probability for each class.
+        """
+        posteriors = []
+        for i in range(count):
+            priori = np.log(prior[i])
+            conditional = np.sum(np.log(get_likelihood(x, mean[i], var[i])))
+            posterior = priori + conditional
+            posteriors.append(posterior)
+        return posteriors
+
+    def get_predictions(vectors, prior, mean, var):
+        """ Compute the prediction for each data vector.
+        """
+        predictions = [classes[np.argmax(get_posteriors(vector, prior, mean, var))] for vector in vectors.to_numpy()]
+        return predictions
+
+    mean, var = get_params(X_train, Y_train)
+    priors = get_priors(X_train, Y_train)
+    Y_predict = get_predictions(X_test, priors, mean, var)
+
+    print('Confusion Matrix\n', confusion_matrix(Y_test, Y_predict))
+    print('Accuracy (in %) = {:.3f}'.format(accuracy_score(Y_test, Y_predict) * 100, '\n'))
+
+
+# solving for real world data
+real_world_data_path = 'real_world_data'
+print('Real World Data')
+solve(real_world_data_path)
--- question2.py
+++ question2.py
@@ -1,113 +1,115 @@
-#Importing necessary libraries 
-import numpy as np #For mathematical operations
-import pandas as pd #For reading the data and handling data
-import matplotlib.pyplot as plt #Plotting and visualising the dataset
-from sklearn.metrics import confusion_matrix #Confusion matrix to get the performance metrics and accuracy
-from sklearn.model_selection import train_test_split #For splitting the dataset into training and testing
+# importing necessary libraries
+import numpy as np  # for mathematical operations
+import pandas as pd  # for reading and handling the data
+import matplotlib.pyplot as plt  # for plotting and visualising the dataset
+from sklearn.metrics import accuracy_score, confusion_matrix  # for obtaining accuracy score and confusion matrix
+from sklearn.model_selection import train_test_split  # for splitting the dataset into training and test data
+
 
 def solve(data_path):
-    #Reading the data and storing the different classes as class1 and class2
-    class1 = pd.read_csv(data_path+"/class1.txt", names=['column1','column2'])
-    class2 = pd.read_csv(data_path+"/class2.txt", names=['column1','column2'])
-    #data is consisting of two features
-    
-    #Adding the new column which is the label/class for that data point
-    #0 label for class1
-    #1 label for class2
+    # reading the data and storing different classes as class1 and class2
+    class1 = pd.read_csv(data_path + "/class1.txt", names=['column1', 'column2'])
+    class2 = pd.read_csv(data_path + "/class2.txt", names=['column1', 'column2'])
+
+    # data consists of two features
+    # adding the new column which is the label/class for that data point
+    # label 0 for class1
+    # label 1 for class2
     class1['class'] = 0
     class2['class'] = 1
-    
-    #Merging the data for both classes
-    merged_data = pd.concat([class1, class2], axis=0)
-    #Resetting the indices for all the entries
+
+    # merging the data for both classes
+    merged_data = pd.concat([class1, class2])
+
+    # resetting the indices for all the entries
     merged_data = merged_data.reset_index(drop=True)
-    #Extracting the class column
+
+    # extracting the 'class' column
     y = merged_data['class']
-    
-    #Dropping the class column 
-    data = merged_data.drop(['class'],axis = 1)
-    #Getting the total class labels
-    classes = np.unique(y)
-    number_of_classes = len(classes)
-    number_of_attrs = data.shape[1]
-    
-    #Splitting the data set into train and test with 20% test size and 80% train size
-    X_train, X_test, y_train, y_test = train_test_split(data, y, test_size=0.2)
-    number_of_rows_train = X_train.shape[0]
-    
-    #Againg merging the class labels to the features
-    train_data = pd.concat([X_train,y_train ], axis=1)
-    
-    #Separating the class datas
-    train_class_0 =  train_data[train_data['class'] == 0]
-    train_class_1 =  train_data[train_data['class'] == 1]
-    
-    #Separating the two features for both the classes
+
+    # dropping the 'class' column
+    data = merged_data.drop(['class'], axis=1)
+
+    # splitting the dataset into train and test data in the ratio of 80% to 20%
+    X_train, X_test, Y_train, Y_test = train_test_split(data, y, test_size=0.2)
+
+    # again merging the class labels to the features
+    train_data = pd.concat([X_train, Y_train], axis=1)
+
+    # separating the class data
+    train_class_0 = train_data[train_data['class'] == 0]
+    train_class_1 = train_data[train_data['class'] == 1]
+
+    # separating the two features for both the classes
     x1_class_0 = train_class_0['column1']
     x2_class_0 = train_class_0['column2']
-    
+
     x1_class_1 = train_class_1['column1']
     x2_class_1 = train_class_1['column2']
-    
-    #Generating the histograms for attribute 1 for both the classes
-    x1_class0, bins1_class0, patches1_class0 = plt.hist(x1_class_0,density=True)
-    x1_class1, bins1_class1, patches1_class1 = plt.hist(x1_class_1,density=True)
+
+    # generating the histograms for attribute 1 for both the classes
+    x1_class0, bins1_class0, patches1_class0 = plt.hist(x1_class_0, density=True)
+    x1_class1, bins1_class1, patches1_class1 = plt.hist(x1_class_1, density=True)
     plt.show()
-    
-    #Generating the histograms for attribute 2 for both the classes
-    x2_class0, bins2_class0, patches2_class0 = plt.hist(x2_class_0,density=True)
-    x2_class1, bins2_class1, patches2_class1 = plt.hist(x2_class_1,density=True)
+
+    # generating the histograms for attribute 2 for both the classes
+    x2_class0, bins2_class0, patches2_class0 = plt.hist(x2_class_0, density=True)
+    x2_class1, bins2_class1, patches2_class1 = plt.hist(x2_class_1, density=True)
     plt.show()
-    
-    
-    #Now predicting the testing data based on information got from the histograms for each data
+
+    # now predicting the test data based on information retrieved from the histograms for each data
     """
     In this predict function, we are calculating:
     Probability of the interval in which the data point is lying(for both the dimensions);
     and then finding resultant probability as product of the two probabilities and finding the
     maximum of both the classes.
     """
+
     def predictClass(data):
         x1 = data['column1']
         x2 = data['column2']
-        prob1_class_0 = 0 #Probability of dimension1 for class 0
-        prob1_class_1 = 0 #Probability of dimension1 for class 1
-        prob2_class_0 = 0 #Probability of dimension2 for class 0
-        prob2_class_1 = 0 #Probability of dimension2 for class 1
-        #Finding the respective probabilities
-        for i in range(x1_class0.shape[0]-1):
-            if x1 >= bins1_class0[i] and x1 <= bins1_class0[i+1]:
-                prob1_class_0 =  x1_class0[i]
-        for i in range(x1_class1.shape[0]-1):
-            if x1 >= bins1_class1[i] and x1 <= bins1_class1[i+1]:
-                prob1_class_1 =  x1_class1[i]
-        for i in range(x2_class0.shape[0]-1):
-            if x2 >= bins2_class0[i] and x2 <= bins2_class0[i+1]:
-                prob2_class_0 =  x2_class0[i]
-        for i in range(x2_class1.shape[0]-1):
-            if x2 >= bins2_class1[i] and x2 < bins2_class1[i+1]:
-                prob2_class_1 =  x2_class1[i]
-        #Finding resultant probabilities for both the classes
-        Probability_class_0 = prob1_class_0*prob2_class_0
-        Probability_class_1 = prob2_class_1*prob1_class_1
+        prob1_class_0 = 0  # Probability of dimension1 for class 0
+        prob1_class_1 = 0  # Probability of dimension1 for class 1
+        prob2_class_0 = 0  # Probability of dimension2 for class 0
+        prob2_class_1 = 0  # Probability of dimension2 for class 1
+
+        # finding the respective probabilities
+        for i in range(x1_class0.shape[0] - 1):
+            if bins1_class0[i] <= x1 <= bins1_class0[i + 1]:
+                prob1_class_0 = x1_class0[i]
+        for i in range(x1_class1.shape[0] - 1):
+            if bins1_class1[i] <= x1 <= bins1_class1[i + 1]:
+                prob1_class_1 = x1_class1[i]
+        for i in range(x2_class0.shape[0] - 1):
+            if bins2_class0[i] <= x2 <= bins2_class0[i + 1]:
+                prob2_class_0 = x2_class0[i]
+        for i in range(x2_class1.shape[0] - 1):
+            if bins2_class1[i] <= x2 <= bins2_class1[i + 1]:
+                prob2_class_1 = x2_class1[i]
+
+        # finding resultant probabilities for both the classes
+        Probability_class_0 = prob1_class_0 * prob2_class_0
+        Probability_class_1 = prob2_class_1 * prob1_class_1
         if Probability_class_0 > Probability_class_1:
             return 0
         else:
             return 1
-    
+
     y_predict = []
     for i in range(X_test.shape[0]):
         class_pred = predictClass(X_test.iloc[i])
         y_predict.append(class_pred)
-        
-    y_predict = np.array(y_predict)
-    confusionmatrix = confusion_matrix(y_test,y_predict)
-    print(confusionmatrix)
 
-#Solving for two data sets
+    # show confusion matrix and accuracy of classifier over the test data
+    y_predict = np.array(y_predict)
+    print('Confusion Matrix\n', confusion_matrix(Y_test, y_predict))
+    print('Accuracy (in %) = {:.3f}'.format(accuracy_score(Y_test, y_predict) * 100, '\n'))
+
+
+# solving for two data sets
 ls_data_path = 'ls_data'
 nls_data_path = 'nls_data'
+print('Linearly Separable Data')
 solve(ls_data_path)
+print('\nNon-linearly Separable Data')
 solve(nls_data_path)
-
-
